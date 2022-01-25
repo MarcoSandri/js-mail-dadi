d853eba2b80e7b5adc7fc09dc14a4a767bce1743
Marco Sandri <sandrimarco98@gmail.com>
2022-01-25 16:58:37 +0100

    Aggiustata email

--- mail/main.js
+++ mail/main.js
@@ -1,13 +1,12 @@
 const emailList = ["bockelboy@att.net" , "pfitza@sbcglobal.net" , "crypt@aol.com" , "jshearer@aol.com" , "parents@msn.com" , "satishr@yahoo.ca" , "hampton@outlook.com" , "hutton@mac.com" , "lamiaemail@gmail.com" , "drezet@me.com" , "ducasse@msn.com" , "jfinke@yahoo.com" , "geekgrl@mac.com" , "doche@verizon.net" , "mrdvt@comcast.net"];
 let myEmail = prompt("Inserisci la tua email");
 
+let login;
 for(let i=0; i < emailList.length; i++) {
 
-    let login;
     if (myEmail == emailList[i]) {
         login = 1;
     } else {
-        login = 0;
     }
 }
 
